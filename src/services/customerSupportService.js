656e4f1753b2aa8f95aa1d2620588eae53b712b5
Nathaniel Oates <natemoates@gmail.com>
2024-03-06 16:29:17 -0500

    Customer Support Service
    Implemented Chatra Help Chat Button that can be placed on any page.

--- src/services/customerSupportService.js
+++ src/services/customerSupportService.js
@@ -1,3 +1,26 @@
-export default class CustomerSupportService {
-    
+'use client';
+
+import React from 'react';
+import { Button, Link, HStack, Box, Flex, Image, Hide, Show } from '@chakra-ui/react';
+import Chatra from '@chatra/chatra';
+
+
+
+export default function CustomerSupportService () {
+    let Chatra = require('@chatra/chatra')
+
+    let config = {
+        setup: {
+            buttonSize: 75
+        },
+        ID: 'Ytpn5ch5ZN3R2D9Qw'
+    }
+
+    Chatra('init', config)  
+    return(
+        <Button>
+            <Link href="#chatraChatExpanded">Chat with us</Link>
+        </Button>
+    )
+
 }
